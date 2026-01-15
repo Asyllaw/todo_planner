e6ba98275936933c606b113db84b8c57ca62948a
Ajoka_Lawrence <110158936+Asyllaw@users.noreply.github.com>
2026-01-16 00:15:51 +0100

    Add files via upload

---
+++ todo_Planner/tasks_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class TasksPage extends StatefulWidget {
+  const TasksPage({Key? key}) : super(key: key);
+
+  @override
+  State<TasksPage> createState() => _TasksPageState();
+}
+
+class _TasksPageState extends State<TasksPage> {
+  double? _deviceHeight, _deviceWidth;
+  String? content;
+  Box? _box;
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight! * 0.1,
+        title: const Text("Daily Planner"),
+      ),
+      body: _tasksWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: displayTaskPop,
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _tasksWidget() {
+    return Container();
+  }
+
+  void displayTaskPop() {
+    showDialog(
+        context: context,
+        builder: (BuildContext _context) {
+          return AlertDialog(
+            title: const Text("Add a todo"),
+            content: TextField(
+              onSubmitted: (value) {
+                setState(() {
+                  print(value);
+                  Navigator.pop(context);
+                });
+              },
+              onChanged: (value) {
+                setState(() {
+                  content = value;
+                });
+              },
+            ),
+          );
+        });
+  }
+}
